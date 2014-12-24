8cfd5378b4ac05d837e5a71d2f3c78856df05961
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2014-12-24 16:46:44 +0000

    Dynamically populating album view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -90,9 +90,88 @@
   globals.require.list = list;
   globals.require.brunch = true;
 })();
-require.register("scripts/app", function(exports, require, module) {
+require.register("scripts/album", function(exports, require, module) {
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtURL: '/images/album-placeholder.png',
+  songs: [
+    { name: 'Blue', length: '4:26'},
+    { name: 'Green', length: '3:14'},
+    { name: 'Red', length: '5:01'},
+    { name: 'Pink', length: '3:21'},
+    { name: 'Magenta', length: '2:15'}
+  ]
+};
+
+var albumMarconi = {
+  name: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+    { name: 'Hello, Operator?', length: '1:01' },
+    { name: 'Ring, ring, ring', length: '5:01' },
+    { name: 'Fits in your pocket', length: '3:21'},
+    { name: 'Can you hear me now?', length: '3:14' },
+    { name: 'Wrong phone number', length: '2:15'}
+  ]
+};
+
+var createSongRow = function(songNumber, songName, songLength){
+  var template =
+    '<tr>'
+    + ' <td class="col-md-1">' + songNumber + '</td>'
+    + ' <td class="col-md-9">' + songName + '</td>'
+    + ' <td class="col-md-2">' + songLength + '</td>'
+    + '</tr>'
+  ;
+  return $(template);
+};
+
+if (document.URL.match(/\/album.html/)) {
+  jQuery(document).ready(function($) {
+    var album = albumPicasso;
+
+    // Update title
+    var $albumTitle = $('.album-title');
+    $albumTitle.text(album.name);
+
+    // Update Album artist
+    var $albumArtist = $('.album-artist');
+    $albumArtist.text(album.artist);
+
+    // Update meta info
+    var $albumMeta = $('.album-meta-info');
+    $albumMeta.text(album.year + " on " + album.label);
+
+    // Update album image
+    var $albumImage = $('.album-image img');
+    $albumImage.attr('src', album.albumArtUrl);
+
+    // Update song list
+    var $songList = $('.album-song-listing');
+    $songList.empty();
+
+    var songs = album.songs;
+    for (var i = 0; i < songs.length; i++) {
+      var songData = songs[i];
+      var $newRow = createSongRow(i + 1, songData.name, songData.length);
+      $songList.append($newRow);
+    };
+
+  });
+};
+
+});
+
+;require.register("scripts/app", function(exports, require, module) {
 require("./landing")
 require("./collection")
+require("./album")
 
 });
 
