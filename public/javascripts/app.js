2e1343e4116e50f3618781316f93cf07362d07f8
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2015-01-16 22:28:26 +0000

    Adding profile

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -259,9 +259,10 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing")
-require("./collection")
-require("./album")
+require("./landing");
+require("./collection");
+require("./album");
+require("./profile");
 
 });
 
@@ -350,6 +351,29 @@ jQuery(document).ready(function($) {
   $('.selling-points .point').hover(hoverOn, hoverOff);
 
 });
+
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+var tabsContainer = ".user-profile-tabs-container";
+var selectTabHandler = function(event){
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden')
+  event.preventDefault();
+}
+
+if(document.URL.match(/\/profile.html/)){
+  jQuery(document).ready(function($) {
+    var $tabs = $(tabsContainer + " a");
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
 
 });
 
